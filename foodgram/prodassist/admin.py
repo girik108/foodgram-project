69ed038d9933900d21f4c3916a02c6df735a4f20
admin <gimatov@list.ru>
2021-02-15 14:01:11 +0400

    Create first models

--- foodgram/prodassist/admin.py
+++ foodgram/prodassist/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Unit, Recipe, Ingredient, RecipesIngredient, Tag
+
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'text',
+                    'pub_date', 'image', 'author', 'time')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class RecipesIngredientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'recipe', 'ingredient',
+                    'unit', 'count',)
+
+
+admin.site.register(Unit, UnitAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipesIngredient, RecipesIngredientAdmin)
+admin.site.register(Tag, TagAdmin)
--- foodgram/prodassist/models.py
+++ foodgram/prodassist/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Unit(models.Model):
+    """Класс Единиц измерения ингредиентов"""
+    name = models.CharField(max_length=100)
+    contraction = models.CharField(max_length=50, default='.')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Ingredient(models.Model):
+    """Класс Ингредиентов"""
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tag(models.Model):
+    """Справочник тегов"""
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'date published', auto_now_add=True, db_index=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='recipes')
+    image = models.ImageField(upload_to='media/images', blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    tag = models.ManyToManyField(Tag)
+    time = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f'{self.name}'
+
+class RecipesIngredient(models.Model):
+    """Класс Ингредиентов в рецептах"""
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   related_name='recipes')
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE,)
+    count = models.PositiveSmallIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('recipe', 'ingredient')
+    
+    def __str__(self):
+        return f'{self.recipe} - {self.ingredient}'

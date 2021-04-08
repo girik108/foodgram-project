db6382891b4bc724bdb1ddf90678df553f70407d
admin <gimatov@list.ru>
2021-04-08 15:07:15 +0400

    Email works

--- foodgram/users/urls.py
+++ foodgram/users/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
-    path('password_change/', views.PassChange.as_view(), name='password_change'),
-    path('password_reset/', views.PassReset.as_view(), name='password_reset'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('signup/', views.SignUp.as_view(),
+         name='signup'),
+    path('password_change/', views.PassChange.as_view(),
+         name='password_change'),
+    path('password_reset/', views.PassReset.as_view(),
+         name='password_reset'),
+    path('login/', views.UserLoginView.as_view(),
+         name='login'),
 ]
